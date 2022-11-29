4bda9dde01134dcd43c853292a1452f92ac1a855
gongxh <94881146+gongxho@users.noreply.github.com>
2022-11-29 13:55:06 +0800

    feat：更新版本时发生资源访问错误。刷新页面以解决浏览器缓存问题 (#9)
    * wip: layout page.
    
    * wip: The layout component is under development.
    
    * feat: The login page is almost complete.
    
    * feat: menu modification and login page completed.
    
    * feat: the system management function is basically completed. (#2)
    
    Co-authored-by: googxh <googxh@111.com>
    
    * feat: base.
    
    * feat: the basic version functionality is complete.
    
    * docs: update Readme.md.
    
    * docs: update Readme.md/2.
    
    * docs: update Readme.md/3.
    
    * ci: Base version integration complete
    
    * feat: add theme layout switching and code optimization
    
    * feat: 更新版本时访问资源错误，刷新页面解决浏览器缓存问题
    
    Co-authored-by: googxh <gxh522@qq.com>
    Co-authored-by: Googxh <49309686+googxho@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import { defineComponent } from 'vue';
+import { InfoFilled } from '@element-plus/icons-vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { defineComponent, markRaw } from 'vue';
 import { RouterView } from 'vue-router';
 
 export default defineComponent({
-  name: 'App',
-  setup() {
-    return () => (
-      <>
-        <RouterView />
-      </>
-    );
-  }
+	name: 'App',
+	setup() {
+		window.addEventListener(
+			'error',
+			(event: any) => {
+				// console.log('error event:', event);
+				if (event.target?.outerHTML?.includes('/assets')) {
+					ElMessageBox.confirm('当前页面有新版本，即将为您更新！', '页面更新提示', {
+						confirmButtonText: 'OK',
+						cancelButtonText: 'Cancel',
+						type: 'info',
+						icon: markRaw(
+							<el-icon>
+								<InfoFilled color="#0080FF" />
+							</el-icon>
+						),
+						draggable: true,
+					})
+						.then(() => {
+							ElMessage({ type: 'success', message: '正在更新！' });
+							setTimeout(() => {
+								window.location.reload();
+							}, 500);
+						})
+						.catch(() => {
+							ElMessage({ type: 'info', message: '已取消' });
+						});
+				}
+				return true;
+			},
+			true // 利用捕获方式
+		);
+		return () => (
+			<>
+				<RouterView />
+			</>
+		);
+	},
 });
